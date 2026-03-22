86911437fd53eb434b1c3974f7fef42924b0344c
Chandu <chandrasekarramella@gmail.com>
2026-03-22 22:08:01 +0530

    updated DB password

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDb = async ()=>{
-    await mongoose.connect("mongodb+srv://chandrasekarramella:chandu123@chandu.dfl03cu.mongodb.net/devTinder")
+    await mongoose.connect("mongodb+srv://chandrasekarramella:chandu@04022006@chandu.dfl03cu.mongodb.net/devTinder")
 };
 
 module.exports = connectDb;
